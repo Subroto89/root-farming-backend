568bda6ac7312668ead4c2ed50a267fc2d174d99
Dhyman Choudhury <dhyman2029@gmail.com>
2025-09-23 11:23:57 +0600

    added MongoDB client setup with environment variables

---
+++ config/db.js
@@ -0,0 +1,15 @@
+// server/config/db.js
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const uri = process.env.MONGO_URL;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+module.exports = client;
